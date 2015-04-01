ce1cd7e0f9e1c09d21833eb359a036b853dabf95
Stephen Done <srdone@gmail.com>
2015-04-01 07:08:17 -0600

    initial fizzBuzz

--- ch02.js
+++ ch02.js
@@ -18,3 +18,31 @@ loopTriangle(4);
 
 console.log('Loop triangle, passing 10 as argument:');
 loopTriangle(10);
+
+console.log('\n\n');
+
+// FizzBuzz
+console.log('FizzBuzz\n');
+
+var fizzBuzz = function fizzBuzz (max) {
+  var maxNumber = max || 100;
+
+  var divisibleByX = function divisibleBy3 (number, x) {
+    return number % x === 0;
+  };
+
+  for (var i = 1; i <= maxNumber; i++) {
+    if (divisibleByX(i, 3) && !divisibleByX(i, 5)) {
+      console.log('Fizz');
+    } else if (divisibleByX(i, 5) && !divisibleByX(i, 3)) {
+      console.log('Buzz');
+    } else if (divisibleByX(i, 5) && divisibleByX(i, 3)) {
+      console.log('FizzBuzz');
+    } else {
+      console.log(i);
+    }
+  }
+};
+
+console.log('fizzBuzz, limit of 20');
+fizzBuzz(20);
